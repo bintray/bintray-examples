73573722d4027e962c39d65a0660a84d0cebe9e7
Sebastian Schuberth <sschuberth@gmail.com>
2015-01-10 23:07:41 +0100

    bash-example: Consistently use two spaces for indentation
    And trim trailing whitespace.

--- bash-example/pushToBintray.sh
+++ bash-example/pushToBintray.sh
@@ -11,22 +11,22 @@ BINTRAY_API_KEY=$2
 BINTRAY_REPO=$3
 PCK_NAME=$4
 PCK_VERSION=$(rpm -qp ${RPM} --qf "%{VERSION}")
-  
-function main() {
-	CURL="curl -u${BINTRAY_USER}:${BINTRAY_API_KEY} -H Content-Type:application/json -H Accept:application/json"
-	if (check_package_exists); then
-		echo "The package ${PCK_NAME} does not exit. It will be created"
-		create_package
-	fi
 
-	deploy_rpm
+function main() {
+  CURL="curl -u${BINTRAY_USER}:${BINTRAY_API_KEY} -H Content-Type:application/json -H Accept:application/json"
+  if (check_package_exists); then
+    echo "The package ${PCK_NAME} does not exit. It will be created"
+    create_package
+  fi
+
+  deploy_rpm
 }
 
 function check_package_exists() {
   echo "Checking if package ${PCK_NAME} exists..."
   package_exists=`[ $(${CURL} --write-out %{http_code} --silent --output /dev/null -X GET  ${API}/packages/${BINTRAY_USER}/${BINTRAY_REPO}/${PCK_NAME})  -eq 200 ]`
-  echo "Package ${PCK_NAME} exists? y:1/N:0 ${package_exists}"   
-  return ${package_exists} 
+  echo "Package ${PCK_NAME} exists? y:1/N:0 ${package_exists}"
+  return ${package_exists}
 }
 
 function create_package() {
@@ -41,7 +41,7 @@ function create_package() {
     \"labels\": [\"bash\", \"example\"]
     }"
   fi
-  
+
   ${CURL} -X POST -d "${data}" ${API}/packages/${BINTRAY_USER}/${BINTRAY_REPO}
 }
 
@@ -51,7 +51,7 @@ function deploy_rpm() {
     ${CURL} -X POST ${API}/content/${BINTRAY_USER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/publish -d "{ \"discard\": \"false\" }"
   else
     echo "[SEVERE] First you should upload your rpm ${RPM}"
-  fi    
+  fi
 }
 
 function upload_content() {
